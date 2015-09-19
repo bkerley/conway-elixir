257c4625342c7448f8dddfc455216bc72ffbb8df
Bryce Kerley <bkerley@brycekerley.net>
2015-09-19 16:58:38 -0400

    a wee pipe in Coord.neighbors

--- lib/coord.ex
+++ lib/coord.ex
@@ -2,13 +2,12 @@ defmodule Coord do
   defstruct x: 0, y: 0
 
   def neighbors(coord = %Coord{x: x, y: y}) do
-    hood = Enum.reduce([x - 1, x, x + 1], HashSet.new, fn(x, row_neighbors) ->
+    Enum.reduce([x - 1, x, x + 1], HashSet.new, fn(x, row_neighbors) ->
       Enum.reduce([y - 1, y, y + 1], row_neighbors, fn(y, col_neighbors) ->
         Set.put(col_neighbors, %Coord{x: x, y: y})
       end)
     end)
-
-    Set.delete(hood, coord)
+    |> Set.delete(coord)
   end
 
   def array_to_coord_set([], coll = %HashSet{}) do
